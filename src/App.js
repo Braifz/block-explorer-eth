bda58e86b3e2af5edd26865f685b6f78b4f98bfe
Braifz <braian94lp@gmail.com>
2023-03-11 23:50:28 -0300

    delete alchemy config

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import { Alchemy, Network } from "alchemy-sdk";
 import { useEffect, useState } from "react";
 import Transactions from "./components/Transactions";
 import Blocks from "./components/Blocks";
 import InputAddress from "./components/InputAddress";
 import Layout from "./components/Layout";
 
+import { alchemy } from "./config/alchemyConfig";
+
 import { useContext } from "react";
 import { ThemeContext } from "./context/theme";
 
 import "./App.css";
-
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
-const settings = {
-  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
-};
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-export const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
